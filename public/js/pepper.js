945a8bb9cad97af269fbae1afb7d00dd7350365b
suki60 <fran.altes@gmail.com>
2019-07-07 17:47:49 +0200

    resize + font roboto

--- public/js/pepper.js
+++ public/js/pepper.js
@@ -1,27 +1,45 @@
 class Pepper {
   constructor(e) {
-    this.$elem = this.createInput(e)
+    this.clientY = e.clientY - 18
+    this.clientX = e.clientX - 1
+    this.$elem = this.createInput()
     this.$elem.focus()
-    this.size = 2
     this.addEvents()
   }
 
-  createInput(e) {
+  createInput() {
     return $('<input/>', {
-      style: `top: ${e.clientY - 18}px; left: ${e.clientX - 1}px`,
+      style: `top: ${this.clientY}px; left: ${this.clientX}px`,
       type: 'text',
-      size: 2
+      size: 1,
     }).appendTo('#white-pepper')
   }
 
-  resize() {
-    this.size++
-    this.$elem.attr('size', this.size)
+  textLength() {
+    return this.$elem.val().length
+  }
+
+  resize(e) {
+    this.$elem.attr('size', this.textLength() + 1)
+  }
+
+  deleteIfSizeZero() {
+    if (this.textLength() == 0) {
+      this.$elem.remove()
+    }
   }
 
   addEvents() {
     this.$elem.on('keydown', (e) => {
       this.resize(e)
+    })
+    
+    this.$elem.on('keyup', (e) => {
+      this.resize(e)
+    })
+
+    this.$elem.on('focusout', () => {
+      this.deleteIfSizeZero()
     })
   }
 }
