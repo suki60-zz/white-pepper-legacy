da0846351433c0ca7d46c3898649434b29fcc83d
suki60 <fran.altes@gmail.com>
2019-09-28 19:52:27 +0200

    feat: add text to migration

--- db/migrations/002_create_peppers_table.rb
+++ db/migrations/002_create_peppers_table.rb
@@ -2,6 +2,7 @@ Sequel.migration do
   change do
     create_table :peppers do
       primary_key :id
+      String :text, text: true
       Fixnum :client_x
       Fixnum :client_y
       foreign_key :user_id, :users, :null => false
